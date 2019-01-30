89d31c9170097456b62a856b3e6acbb32b7a59bd
seemie <chenios@foxmail.com>
2019-01-30 23:27:50 +0800

    简化代码

--- src/main/java/com/fileup/bitsyntax/CCFLib.java
+++ src/main/java/com/fileup/bitsyntax/CCFLib.java
@@ -1,12 +1,11 @@
 package com.fileup.bitsyntax;
 
+import static com.fileup.bitsyntax.function.BitConvertLib.*;
+import static com.fileup.util.BytesUtil.*;
 import java.util.HashMap;
 import java.util.Map;
-
 import com.fileup.bitsyntax.function.FromBytes;
 import com.fileup.bitsyntax.function.ToBytes;
-
-
 
 /**© 2015-2017 Chenxj Copyright
  * 类    名：CCFLib
@@ -15,7 +14,23 @@ import com.fileup.bitsyntax.function.ToBytes;
  * 邮    箱：chenios@foxmail.com
  * 日    期：2017年9月16日-上午10:31:05
  */
-public interface CCFLib {
+public abstract class CCFLib {
 	public static final Map<String, FromBytes>fromLib=new HashMap<>();
 	public static final Map<String, ToBytes>toLib=new HashMap<>();
+	static {
+		fromLib.put("I", (o)->{return bytes2integer((byte[]) o);});
+		fromLib.put("U8", (o)->{return bytes2utf8((byte[]) o);});
+		fromLib.put("CCID", (o)->{return bytesToCCString((byte[]) o);});
+		fromLib.put("HEX", (o)->{return bytesToHexString((byte[]) o);});
+		fromLib.put("L", (o)->{return bytes2long((byte[]) o);});
+		fromLib.put("F", (o)->{return bytes2float((byte[]) o);});
+		fromLib.put("D", (o)->{return bytes2double((byte[]) o);});
+		toLib.put("I", (o,l)->{if(l==null||l<1) {l=4;}return integer2bytes(o, l);});
+		toLib.put("U8", (o,l)->{return utf82bytes(o);});
+		toLib.put("CCID", (o,l)->{return ccStringToBytes((String)o);});
+		toLib.put("HEX", (o,l)->{return hexStringToBytes((String)o);});
+		toLib.put("L", (o,l)->{if(l==null||l<1) {l=8;}return long2bytes(o, l);});
+		toLib.put("F", (o,l)->{if(l==null||l<1) {l=4;}return float2bytes(o, l);});
+		toLib.put("D", (o,l)->{if(l==null||l<1) {l=8;}return double2bytes(o, l);});
+	}
 }
--- src/main/java/com/fileup/util/BtxUtil.java
+++ src/main/java/com/fileup/util/BtxUtil.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.fileup.bitsyntax.CCBitsyntax;
-import com.fileup.bitsyntax.CCFLib;
-import com.fileup.bitsyntax.function.BitConvertLib;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -15,23 +13,6 @@ import io.netty.buffer.Unpooled;
 
 public class BtxUtil {
 	private static CCBitsyntax btx=new CCBitsyntax("hd:1/I,(hd){0:{id:16/HEX,size:8/L,suffix/U8,'#',type/U8,'#',name/U8},1:{indexStart:8/L,indexEnd:8/L,completePercent:4/I},2:{data}}");
-	static {
-		CCFLib.fromLib.put("I", (o)->{return BitConvertLib.bytes2integer((byte[]) o);});
-		CCFLib.fromLib.put("U8", (o)->{return BitConvertLib.bytes2utf8((byte[]) o);});
-		CCFLib.fromLib.put("CCID", (o)->{return BytesUtil.bytesToCCString((byte[]) o);});
-		CCFLib.fromLib.put("HEX", (o)->{return BytesUtil.bytesToHexString((byte[]) o);});
-		CCFLib.fromLib.put("L", (o)->{return BitConvertLib.bytes2long((byte[]) o);});
-		CCFLib.fromLib.put("F", (o)->{return BitConvertLib.bytes2float((byte[]) o);});
-		CCFLib.fromLib.put("D", (o)->{return BitConvertLib.bytes2double((byte[]) o);});
-		
-		CCFLib.toLib.put("I", (o,l)->{if(l==null||l<1) {l=4;}return BitConvertLib.integer2bytes(o, l);});
-		CCFLib.toLib.put("U8", (o,l)->{return BitConvertLib.utf82bytes(o);});
-		CCFLib.toLib.put("CCID", (o,l)->{return BytesUtil.ccStringToBytes((String)o);});
-		CCFLib.toLib.put("HEX", (o,l)->{return BytesUtil.hexStringToBytes((String)o);});
-		CCFLib.toLib.put("L", (o,l)->{if(l==null||l<1) {l=8;}return BitConvertLib.long2bytes(o, l);});
-		CCFLib.toLib.put("F", (o,l)->{if(l==null||l<1) {l=4;}return BitConvertLib.float2bytes(o, l);});
-		CCFLib.toLib.put("D", (o,l)->{if(l==null||l<1) {l=8;}return BitConvertLib.double2bytes(o, l);});
-	}
 	public static CCBitsyntax btx() {
 		return btx;
 	}
